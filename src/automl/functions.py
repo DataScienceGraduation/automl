623378351641093a9f9b82ff5da4e413f5c6d4bb
Ahmed Khaled <ahmedkhaledmoghazy10@gmail.com>
2025-05-02 18:37:41 +0300

    Add debug print statements in createPipeline function for tracing execution flow during pipeline creation.

--- src/automl/functions.py
+++ src/automl/functions.py
@@ -8,16 +8,22 @@ from .preprocess.TimeSeriesPreprocessor import TimeSeriesPreprocessor
 def createPipeline(df: DataFrame, target_variable: str = None, task=None) -> Pipeline:
     print("Creating the pipeline")
     if task == "TimeSeries":
+        print("10")
         preprocessor = TimeSeriesPreprocessor(target_column=target_variable)
+        print("11")
     else:
         preprocessor = Preprocess(target_variable=target_variable)
+        print("12")
 
     # For clustering, we don't need feature engineering with target variable
     if task == "Clustering":
+        print("13")
         pipeline = Pipeline([
             ('preprocess', preprocessor)
         ])
+        print("14")
     else:
+        print("15")
         pipeline = Pipeline([
             ('preprocess', preprocessor),
             ('feature_engineer', FeatureEngineer(target_variable=target_variable)),
@@ -25,8 +31,12 @@ def createPipeline(df: DataFrame, target_variable: str = None, task=None) -> Pip
 
     # For clustering, we don't need to fit with target variable
     if task == "Clustering":
+        print("16")
         pipeline.fit(df)
+        print("17")
     else:
+        print("18")
         pipeline.fit(df, df[target_variable])
+        print("19")
 
     return pipeline
